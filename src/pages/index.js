38e606bc97a141d2b88977ae5bbb0ac5b813b19d
rafael-ferraz-s <rafael.ferraz@picpay.com>
2024-11-12 00:03:37 -0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { Redirect } from 'react-router-dom';
 export default function Home() {
   console.log('Redirecting to /docs/intro.md');
-  return <Redirect to="/docs/intro" />;
+  return <Redirect to="/doc/docs/intro" />;
 }

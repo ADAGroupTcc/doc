8e40d2d9407af5752a264f5ff035e2c599fb2ca7
rafael-ferraz-s <rafael.ferraz@picpay.com>
2024-11-12 00:04:27 -0300

    Deploy website - based on 38e606bc97a141d2b88977ae5bbb0ac5b813b19d

---
+++ assets/js/c4f5d8e4.6877ea90.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[634],{192:(e,t,o)=>{o.r(t),o.d(t,{default:()=>n});var s=o(6347),c=o(4848);function n(){return console.log("Redirecting to /docs/intro.md"),(0,c.jsx)(s.rd,{to:"/doc/docs/intro"})}}}]);
